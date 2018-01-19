8c596a039292b4bbdf03f1f0d20af0bfcc181738
Sebastiaan Mulders <s.p.mulders@tudelft.nl>
2018-01-19 13:12:17 +0100

    Updates NREL5MW model and linearization, added check for existence inputfiles-folder

--- RunGUI.m
+++ RunGUI.m
@@ -2,5 +2,10 @@ clear all
 close all
 addpath(genpath('subfunctions'));
 
+% Create the inputfiles folder if it does not exist
+if not(exist('.\subfunctions\inputfiles', 'dir'))
+    mkdir '.\subfunctions\inputfiles'
+end
+
 % FAST GUI for the course AE4W09 - Wind Turbine Design
 run('WindTurbineDesign.m');
