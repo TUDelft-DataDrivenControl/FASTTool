682503370764ae574110ab1ac95cc3b31abfb6b0
Unknown <s.p.mulders@tudelft.nl>
2020-01-10 17:23:06 +0100

    Solves a problem where in some cases BModes cannot write its output file

--- FASTTool.m
+++ FASTTool.m
@@ -8,5 +8,10 @@ if not(exist(['.' filesep 'subfunctions' filesep 'inputfiles'], 'dir'))
     eval(['mkdir .' filesep 'subfunctions' filesep 'inputfiles'])
 end
 
+% Remove Deny-permissions from the inputfiles-folder, to avoid problems
+% with BModes not being able to write its output file
+[~,~] = system(['icacls subfunctions' filesep 'inputfiles /inheritance:d']);
+[~,~] = system(['icacls subfunctions' filesep 'inputfiles /remove:d Everyone']);
+
 % FASTTool for the course AE4W09 - Wind Turbine Design
 run('WindTurbineDesign.m');
